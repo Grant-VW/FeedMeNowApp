ef6b86c7ee8eb69978ccf4573117cc8a33bd2dc7
GrantVW <grant.williams2711@gmail.com>
2020-10-29 20:24:47 +0200

    Displayin JSON in front end [WIP] - to do import a ViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,23 +6,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FeedMeNow.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace FeedMeNow.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
 
+        public HomeController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        /*
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        */
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult RestuarantList(string foodAndLocation)
+        {
+            //'build' string for JSON file location
+            string contentRootPath = _env.ContentRootPath;
+            string filePath = contentRootPath + @"\AppMockData\SampleData.json";
+            string jsonString = System.IO.File.ReadAllText(filePath);
+            var restuarants = JsonConvert.DeserializeObject<List<Restuarant>>(jsonString);
+
+            //string foodAndLocation = form["foodAndLocation"].ToString();
+            //ViewData["Message"] = "You searched for " + foodAndLocation;
+
+            ViewData["Message"] = restuarants;
+            return View(); 
+        }
         public IActionResult Privacy()
         {
             return View();
